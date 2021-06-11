79b3d82f58721360944a3c734f4484cd24acc529
arthurgehrke <arthurgehrke07@gmail.com>
2021-06-11 19:10:10 -0300

    change fzf config

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,63 +1,25 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/arthurgehrke/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/arthurgehrke/.fzf/bin"
+if [[ ! "$PATH" == */home/arthurgehrke/.local/share/dotfiles/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/arthurgehrke/.local/share/dotfiles/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/arthurgehrke/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/arthurgehrke/.local/share/dotfiles/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/arthurgehrke/.fzf/shell/key-bindings.zsh"
+source "/home/arthurgehrke/.local/share/dotfiles/fzf/shell/key-bindings.zsh"
 
-# fd - cd to selected directory
-fd() {
-  local dir
-  dir=$(find ${1:-*} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
-}
+export FZF_DEFAULT_COMMAND='rg --files --hidden'  # Respect .gitignore, show hidden files
+export FZF_DEFAULT_OPTS='--bind ctrl-l:select-all,ctrl-n:toggle+up,ctrl-f:preview-page-down,ctrl-b:preview-page-up,::abort'
+export FZF_COMPLETION_OPTS='-m'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"    # Respect .gitignore for ^t
+export FZF_CTRL_T_OPTS="--preview 'bat --color \"always\" {}'"
+export FZF_TMUX_HEIGHT=70%
 
-# fe [FUZZY PATTERN] - Open the selected file with the default editor
-#   - Bypass fuzzy finder if there's only one match (--select-1)
-#   - Exit if there's no match (--exit-0)
-fe() {
-  local file
-  file=$(fzf --query="$1" --select-1 --exit-0)
-  [ -n "$file" ] && ${EDITOR:-vim} "$file"
-}
-
-
-# fda - including hidden directories
-fda() {
-  local dir
-  dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
-}
-
-# cdf - cd into the directory of the selected file
-cdf() {
-   local file
-   local dir
-   file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
-}
-
-# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
-fbr() {
-  local branches branch
-  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
-
-# fcoc - checkout git commit
-fcoc() {
-  local commits commit
-  commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf --tac +s +m -e) &&
-  git checkout $(echo "$commit" | sed "s/ .*//")
-}
-
-
+#export FZF_DEFAULT_COMMAND="fd --type f --hidden"
+#export FZF_CTRL_T_COMMAND="fd --type f --hidden"
+#export FZF_ALT_C_COMMAND="fd --type d --hidden"
+#export FZF_DEFAULT_OPTS='--color fg:-1,bg:-1,hl:33,fg+:235,bg+:-1,hl+:33 --color info:136,prompt:136,pointer:230,marker:230,spinner:136'
