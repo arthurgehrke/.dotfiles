80b1aa27f91b22daccc377fd18404a1c0eaba78f
Arthur Gehrke <arthurgehrke07@gmail.com>
2024-03-19 02:00:23 -0300

    fix(dotfiles): general

--- .fzf.zsh
+++ .fzf.zsh
@@ -3,4 +3,4 @@
 # ---------
 # Key bindings
 # ------------
-export RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --hidden --type file"
+export RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,~/dotfiles/**} --hidden --type file"
